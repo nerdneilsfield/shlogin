0e0387d264c82f9bdb7dd2510918eb1b9df7e45f
dengqi <dengqi935@outlook.com>
2024-10-21 19:14:23 +0800

    fix: update logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,63 +1,101 @@
 package logger
 
 import (
-	"os"
-	"path/filepath"
-
-	"github.com/inconshreveable/log15"
+	"go.uber.org/zap"
+	_ "go.uber.org/zap" // 防止被 go mod tidy 移除
+	"go.uber.org/zap/zapcore"
 )
 
-var (
-	logger        = NewDefaultLogger()
-	logLevel      = log15.LvlInfo
-	stdoutHandler = log15.StreamHandler(os.Stdout, log15.LogfmtFormat())
-	outToFile     = false
-	logFile       = ""
-)
-
-func SetLogger(l *log15.Logger) {
-	logger = l
+type Logger struct {
+	structLogger  *zap.Logger
+	sugaredLogger *zap.SugaredLogger
 }
 
-func GetLogger() *log15.Logger {
-	return logger
-}
+var logger = NewLogger(false, false, "")
 
-func NewDefaultLogger() *log15.Logger {
-	logger := log15.New()
-	logger.SetHandler(log15.StreamHandler(os.Stdout, log15.LogfmtFormat()))
-	return &logger
-}
+func NewLogger(verbose bool, saveToFile bool, logFilePath string) *Logger {
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
 
-func CheckLogFileDirExistOrCreate(logFile string) {
-	// get dir from logFile
-	dir := filepath.Dir(logFile)
-	if _, err := os.Stat(dir); os.IsNotExist(err) {
-		os.MkdirAll(dir, 0o755)
-	}
-}
+	var structLogger *zap.Logger
+	var sugaredLogger *zap.SugaredLogger
+	if verbose && saveToFile {
+		core := zapcore.NewCore(
+			zapcore.NewJSONEncoder(encoderConfig),
+			zapcore.AddSync(logFilePath),
+			zap.LevelEnablerFunc(func(level zapcore.Level) bool {
+				return level >= zapcore.DebugLevel
+			}),
+		)
+		structLogger = zap.New(core)
 
-// Change log level
-func UpdateLogLevel(level log15.Lvl) {
-	logLevel = level
-	logger_ := log15.New()
-	stdoutHandler = log15.StreamHandler(os.Stdout, log15.LogfmtFormat())
-	stdoutHandler = log15.LvlFilterHandler(logLevel, stdoutHandler)
-	if outToFile && logFile != "" {
-		outToFileHandler := log15.Must.FileHandler(logFile, log15.LogfmtFormat())
-		outToFileHandler = log15.LvlFilterHandler(logLevel, outToFileHandler)
-		// 修改为追加模式
-		outToFileHandler = log15.Must.FileHandler(logFile, log15.LogfmtFormat())
-		logger_.SetHandler(log15.MultiHandler(stdoutHandler, outToFileHandler))
+		config := zap.Config{
+			Level:         zap.NewAtomicLevelAt(zapcore.DebugLevel),
+			Development:   true,
+			Encoding:      "json",
+			EncoderConfig: encoderConfig,
+			OutputPaths:   []string{"stdout"},
+		}
+		temp_logger, _ := config.Build()
+		sugaredLogger = temp_logger.Sugar()
 	} else {
-		logger_.SetHandler(stdoutHandler)
+		structLogger = nil
+		config := zap.Config{
+			Level:         zap.NewAtomicLevelAt(zapcore.InfoLevel),
+			Development:   false,
+			Encoding:      "json",
+			EncoderConfig: encoderConfig,
+			OutputPaths:   []string{"stdout"},
+		}
+		temp_logger, _ := config.Build()
+		sugaredLogger = temp_logger.Sugar()
 	}
-	SetLogger(&logger_)
+
+	logger_ := &Logger{
+		structLogger:  structLogger,
+		sugaredLogger: sugaredLogger,
+	}
+	defer logger_.structLogger.Sync()
+	defer logger_.sugaredLogger.Sync()
+	return logger_
 }
 
-func SaveLogToFile(logFile_ string) {
-	logFile = logFile_
-	CheckLogFileDirExistOrCreate(logFile)
-	outToFile = true
-	UpdateLogLevel(logLevel)
+func SetLogger(logger_ *Logger) {
+	logger = logger_
+}
+
+func (l *Logger) Info(msg string, args ...interface{}) {
+	if l.structLogger != nil {
+		l.structLogger.Info(msg, args...)
+	}
+	l.sugaredLogger.Infof(msg, args...)
+}
+
+func (l *Logger) Error(msg string, args ...interface{}) {
+	if l.structLogger != nil {
+		l.structLogger.Error(msg, args...)
+	}
+	l.sugaredLogger.Errorf(msg, args...)
+}
+
+func (l *Logger) Fatal(msg string, args ...interface{}) {
+	if l.structLogger != nil {
+		l.structLogger.Fatal(msg, args...)
+	}
+	l.sugaredLogger.Fatalf(msg, args...)
+}
+
+func (l *Logger) Debug(msg string, args ...interface{}) {
+	if l.structLogger != nil {
+		l.structLogger.Debug(msg, args...)
+	}
+	l.sugaredLogger.Debugf(msg, args...)
+}
+
+func (l *Logger) Panic(msg string, args ...interface{}) {
+	if l.structLogger != nil {
+		l.structLogger.Panic(msg, args...)
+	}
+	l.sugaredLogger.Panicf(msg, args...)
 }
